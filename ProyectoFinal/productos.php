00488a86083532895ebe0305e779b975e22b01a0
Shad0wman11 <osmar_935@hotmail.com>
2023-12-05 21:56:47 -0600

    cambios

--- ProyectoFinal/productos.php
+++ ProyectoFinal/productos.php
@@ -77,14 +77,13 @@ else{
     </div>
     
 </div>
-<?php
-        include 'footer.php';
-    ?>
+
     <div class="loader-wrapper">
     <div class="loader"></div>
     <p>Cargando...</p>
   </div>
 </body>
+
 </html>
 <!-- SCRITPS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script></body>
